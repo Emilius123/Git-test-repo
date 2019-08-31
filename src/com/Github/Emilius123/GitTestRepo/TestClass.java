1de95c0cfe904d65560401c76b6f00634875f26e
Emilius123 <47276235+Emilius123@users.noreply.github.com>
2019-08-31 18:19:37 +0200

    Added constructor and dosomething function.

--- src/com/Github/Emilius123/GitTestRepo/TestClass.java
+++ src/com/Github/Emilius123/GitTestRepo/TestClass.java
@@ -1,5 +1,24 @@
+/**
+  TestClass
+  Can be cool
+ */
 package com.Github.Emilius123.GitTestRepo;
 
 public class TestClass {
 	private boolean isACoolTest;
+	
+	public TestClass(boolean cool) {
+		this.isACoolTest = cool;
+	}
+	
+	public void doSomething() {
+		if(this.isACoolTest == true) {
+			//This is a cool test
+			System.out.println("I'm a cool test!");
+		}else {
+			//This is a not cool test
+			System.out.println("I'm a test!");
+		}
+	}
+	
 }
