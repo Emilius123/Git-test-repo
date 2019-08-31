794eed67cbedfc6aa71a079018152bb7c8fcdd8c
Emilius123 <47276235+Emilius123@users.noreply.github.com>
2019-08-31 19:23:46 +0200

    Initial example comment

---
+++ Examples/Not cool/com/Github/Emilius123/GitTestRepoExamples/Main.java
@@ -0,0 +1,14 @@
+package com.Github.Emilius123.GitTestRepoExamples;
+
+import com.Github.Emilius123.GitTestRepo.Coolness;
+import com.Github.Emilius123.GitTestRepo.TestClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Not cool
+		TestClass c = new TestClass(Coolness.NOT_COOL);
+		c.doSomething();
+	}
+
+}
