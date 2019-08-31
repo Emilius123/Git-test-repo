4b2e005b5819cf08269d5764432dcb369cd15577
Emilius123 <47276235+Emilius123@users.noreply.github.com>
2019-08-31 18:43:34 +0200

    Now using Coolness Enum

--- src/com/Github/Emilius123/GitTestRepo/TestClass.java
+++ src/com/Github/Emilius123/GitTestRepo/TestClass.java
@@ -5,19 +5,22 @@
 package com.Github.Emilius123.GitTestRepo;
 
 public class TestClass {
-	private boolean isACoolTest;
+	private Coolness Coolness;
 	
-	public TestClass(boolean cool) {
-		this.isACoolTest = cool;
+	public TestClass(Coolness cool) {
+		this.Coolness = cool;
 	}
 	
 	public void doSomething() {
-		if(this.isACoolTest == true) {
-			//This is a cool test
-			System.out.println("I'm a cool test!");
-		}else {
-			//This is a not cool test
-			System.out.println("I'm a test!");
+		if(this.Coolness == Coolness.COOL) {
+			//Cool
+			System.out.println("WOWOOWOWOW I'M COOL!");
+		}else if(this.Coolness == Coolness.KINDA_COOL) {
+			//Kinda cool
+			System.out.println("yo im kinda cool");
+		}else if(this.Coolness == Coolness.NOT_COOL) {
+			//Not cool
+			System.out.println("im not cool, :(");
 		}
 	}
 	
