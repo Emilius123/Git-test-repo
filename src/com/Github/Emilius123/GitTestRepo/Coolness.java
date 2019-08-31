3d35f4e2e400f90eca76cae2de7c3b277d475cb9
Emil <emiluuu40@gmail.com>
2019-08-31 20:00:55 +0200

    Expanded comment

--- src/com/Github/Emilius123/GitTestRepo/Coolness.java
+++ src/com/Github/Emilius123/GitTestRepo/Coolness.java
@@ -1,6 +1,7 @@
 package com.Github.Emilius123.GitTestRepo;
 //Coolness is the only thing that matters.
 //That's why we added this Enum.
+//With this you will always know how cool something is
 public enum Coolness {
 	OMG_SO_ULTRA_HYDRA_MUCH_COOL,
 	COOL,
