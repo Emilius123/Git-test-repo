fd3eeedc44ae11d34e740dcf1dc1801eb454015b
Emil <emiluuu40@gmail.com>
2019-08-31 22:39:19 +0200

    Added feeling and getter and setter for feeling

--- src/com/Github/Emilius123/GitTestRepo/TestClass.java
+++ src/com/Github/Emilius123/GitTestRepo/TestClass.java
@@ -7,30 +7,42 @@ package com.Github.Emilius123.GitTestRepo;
 
 public class TestClass {
 	private Coolness Coolness;
-	
+	private Feeling Feeling;
 	public TestClass(Coolness cool) {
 		this.Coolness = cool;
 	}
 	
 	public Feeling doSomething() {
-		if(this.Coolness == Coolness.COOL) {
+		if(this.Coolness == com.Github.Emilius123.GitTestRepo.Coolness.COOL) {
 			//Cool
 			System.out.println("WOWOOWOWOW I'M COOL!");
-			return Feeling.Awseome;
-		}else if(this.Coolness == Coolness.KINDA_COOL) {
+			this.setFeeling(com.Github.Emilius123.GitTestRepo.Feeling.Awseome);
+			return com.Github.Emilius123.GitTestRepo.Feeling.Awseome;
+		}else if(this.Coolness == com.Github.Emilius123.GitTestRepo.Coolness.KINDA_COOL) {
 			//Kinda cool
 			System.out.println("yo im kinda cool");
-			return Feeling.Kinda_Good;
-		}else if(this.Coolness == Coolness.NOT_COOL) {
+			this.setFeeling(com.Github.Emilius123.GitTestRepo.Feeling.Kinda_Good);
+			return com.Github.Emilius123.GitTestRepo.Feeling.Kinda_Good;
+		}else if(this.Coolness == com.Github.Emilius123.GitTestRepo.Coolness.NOT_COOL) {
 			//Not cool
 			System.out.println("im not cool, :*(");
-			return Feeling.Me_cri;
-		}else if(this.Coolness == Coolness.OMG_SO_ULTRA_HYDRA_MUCH_COOL) {
+			this.setFeeling(com.Github.Emilius123.GitTestRepo.Feeling.Me_cri);
+			return com.Github.Emilius123.GitTestRepo.Feeling.Me_cri;
+		}else if(this.Coolness == com.Github.Emilius123.GitTestRepo.Coolness.OMG_SO_ULTRA_HYDRA_MUCH_COOL) {
 			//OMG SO ULTRA HYDRA MUCH COOL
 			System.out.println("NO WAY IM SO OMG SO ULTRA HYDRA MUCH COOL OMG SO AWSEOME ");
-			return Feeling.OMG_SO_SUPER_ULTRA_HYDRA_COOL;
+			this.setFeeling(com.Github.Emilius123.GitTestRepo.Feeling.OMG_SO_SUPER_ULTRA_HYDRA_COOL);
+			return com.Github.Emilius123.GitTestRepo.Feeling.OMG_SO_SUPER_ULTRA_HYDRA_COOL;
 		}
 		return null;
+	}
+
+	public Feeling getFeeling() {
+		return Feeling;
+	}
+
+	public void setFeeling(Feeling feeling) {
+		Feeling = feeling;
 	}
 	
 	
