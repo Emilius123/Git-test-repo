c3a531c0ee5f52ca31c73cae88a35542b92eafcc
Emil <emiluuu40@gmail.com>
2019-08-31 22:33:45 +0200

    Implemented feeling enum

--- src/com/Github/Emilius123/GitTestRepo/TestClass.java
+++ src/com/Github/Emilius123/GitTestRepo/TestClass.java
@@ -12,20 +12,25 @@ public class TestClass {
 		this.Coolness = cool;
 	}
 	
-	public void doSomething() {
+	public Feeling doSomething() {
 		if(this.Coolness == Coolness.COOL) {
 			//Cool
 			System.out.println("WOWOOWOWOW I'M COOL!");
+			return Feeling.Awseome;
 		}else if(this.Coolness == Coolness.KINDA_COOL) {
 			//Kinda cool
 			System.out.println("yo im kinda cool");
+			return Feeling.Kinda_Good;
 		}else if(this.Coolness == Coolness.NOT_COOL) {
 			//Not cool
 			System.out.println("im not cool, :*(");
+			return Feeling.Me_cri;
 		}else if(this.Coolness == Coolness.OMG_SO_ULTRA_HYDRA_MUCH_COOL) {
 			//OMG SO ULTRA HYDRA MUCH COOL
 			System.out.println("NO WAY IM SO OMG SO ULTRA HYDRA MUCH COOL OMG SO AWSEOME ");
+			return Feeling.OMG_SO_SUPER_ULTRA_HYDRA_COOL;
 		}
+		return null;
 	}
 	
 	
