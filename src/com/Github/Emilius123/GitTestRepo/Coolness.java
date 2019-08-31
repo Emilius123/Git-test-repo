497aaea19963e76abbb4e28f4ed60be74ead0acc
Emil <emiluuu40@gmail.com>
2019-08-31 22:41:57 +0200

    Made a large comment out of 3 single liners

--- src/com/Github/Emilius123/GitTestRepo/Coolness.java
+++ src/com/Github/Emilius123/GitTestRepo/Coolness.java
@@ -1,7 +1,9 @@
+/**
+  Coolness is the only thing that matters.
+  That's why we created this Enum.
+  With this you will always know how cool something is
+ */
 package com.Github.Emilius123.GitTestRepo;
-//Coolness is the only thing that matters.
-//That's why we added this Enum.
-//With this you will always know how cool something is
 public enum Coolness {
 	OMG_SO_ULTRA_HYDRA_MUCH_COOL,
 	COOL,
