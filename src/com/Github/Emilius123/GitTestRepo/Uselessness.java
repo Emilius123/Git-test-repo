9bcecf391b91b0b3260f3dea3b8d337dae6eb646
Emil <emiluuu40@gmail.com>
2019-09-01 09:17:02 +0200

    Added uselessness enum

---
+++ src/com/Github/Emilius123/GitTestRepo/Uselessness.java
@@ -0,0 +1,11 @@
+/**
+  Lots of Testclasses are useless.
+  You always need to know how useless something really is 
+  That's why we created this Enum.
+ */
+package com.Github.Emilius123.GitTestRepo;
+
+public enum Uselessness {
+	Useless,
+	Very_Useless;
+}
